95411342157f0d067627f3846c26eea198fcc6f6
Aiman-Abdullah <32886468+Aiman-Abdullah@users.noreply.github.com>
2022-01-20 17:47:52 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'aiman-my-sql-server.mysql.database.azure.com'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'sampledb'),
-      user: env('DATABASE_USERNAME', 'aiman@aiman-my-sql-server'),
-      password: env('DATABASE_PASSWORD', 'Aloved12!'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'password'),
       ssl: env.bool('DATABASE_SSL', true),
     },
 
